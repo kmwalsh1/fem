aa9c401c8f96e0ccb63000eb206bee8b4b4fe029
Ningrui Li <ningrui.li@duke.edu>
2014-09-05 15:06:00 -0400

    added conversion of experimentally measured impulse response variables into numeric arrays. also added print statement to show what function was used to create the transducer handle and impulse response

--- field/jsonToProbe.m
+++ field/jsonToProbe.m
@@ -19,13 +19,20 @@ end
 no_elements = (FIELD_PARAMS.focus(3)/FIELD_PARAMS.Fnum)/pitch;
 no_elements = floor(no_elements);
 
-if (no_elements > probe.no_elements)
+if (exist('probe.no_elements', 'var') && no_elements > probe.no_elements)
     no_elements = probe.no_elements;
 end
 
+% the function fromjson converts json arrays into matlab cell arrays, so
+% here we convert them into a numeric array.
+probe.impulse_response.time = cell2mat(probe.impulse_response.time);
+probe.impulse_response.voltage = cell2mat(probe.impulse_response.voltage);
+
 % Getting transducer handle and impulse response
-eval(probe.commands.Th)
-eval(probe.commands.impulseResponse)
+fprintf('%s\n', probe.commands.Th)
+eval(probe.commands.Th) % define Th
+fprintf('%s\n', probe.commands.impulseResponse)
+eval(probe.commands.impulseResponse) % define impulseResponse
 
 % need to change formatExpImpResp to take in json formatted time and
 % voltage data for the impulse response, rather than the .pul file
