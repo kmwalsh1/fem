d4d7d8241fbaee036fcabba9765a11406cd4c1ef
Mark Palmeri <mlp6@duke.edu>
2014-10-06 19:36:35 -0400

    augment comments

--- field/checkUniform.m
+++ field/checkUniform.m
@@ -1,12 +1,15 @@
 function [isUniform] = checkUniform(measurementPoints)
 % function [isUniform] = checkUniform(measurementPoints)
 %
-% check to see if mesh is linear or nonlinear. if mesh is nonlinear, this means
-% that force scaling needs to be done
+% If mesh is non-uniform, then need to use calculated element volumes to
+% scale point loads (otherwise simple element volume calculation can be done
+% for all uniform elements).
 %
+
 x = unique(measurementPoints(:, 1));
 y = unique(measurementPoints(:, 2));
 z = unique(measurementPoints(:, 3));
+
 isUniform = all(abs(diff(x)/(x(2)-x(1))-1 < 10^-9)) &&...
             all(abs(diff(y)/(y(2)-y(1))-1 < 10^-9)) &&...
             all(abs(diff(z)/(z(2)-z(1))-1 < 10^-9));
